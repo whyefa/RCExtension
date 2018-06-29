a5553cebd726a210279999c81664ac87fe333626
whyefa <whyefa@163.com>
2018-06-29 10:20:25 +0800

    clean up

--- RCExtension/ViewController.m
+++ RCExtension/ViewController.m
@@ -10,7 +10,7 @@
 #import "RCExtension/NSString+Extension/NSString+RCExtension.h"
 #import "RCExtension/NSObject+Extension/NSObject+Extension.h"
 #import "RCExtension/RCKit/RCDatePickerView.h"
-#import "RCPopoverView.h"
+
 
 @interface ViewController ()
 
@@ -28,30 +28,6 @@
     self.label.attributedText = aStr;
 }
 
-- (IBAction)popMenu:(id)sender {
-    [self showMenuOfStyle:RCPopoverViewBorderStyleDark];
-}
-- (IBAction)shadowMenu:(id)sender {
-    [self showMenuOfStyle:RCPopoverViewBorderStyleShadow];
-}
-- (IBAction)boarderMenu:(id)sender {
-    [self showMenuOfStyle:RCPopoverViewBorderStyleBoarder];
-}
 
-- (void)showMenuOfStyle:(RCPopoverViewBorderStyle)borderStyle {
-    RCPopoverView *v = [RCPopoverView popoverView];
-    v.borderStyle = borderStyle;
-    v.arrowStyle = RCPopoverViewArrowStyleTriangle;
-    v.separatorColor = UIColor.redColor;
-    v.shadowColor = [UIColor colorWithWhite:0 alpha:0.6];
-    v.shadowParam = (RCPopoverShadow){0.3, 3, CGSizeZero};
-    RCPopoverAction *a = [RCPopoverAction actionWithTitle:@"你好" handler:^(RCPopoverAction *action) {
-        NSLog(@"nihao");
-    }];
-    RCPopoverAction *b = [RCPopoverAction actionWithTitle:@"再见" handler:^(RCPopoverAction *action) {
-        NSLog(@"再见");
-    }];
-    [v showToPoint:CGPointMake(20, self.view.frame.size.height-50) withActions:@[a, b]];
-}
 
 @end
